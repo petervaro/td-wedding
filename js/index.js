0896513ec1ea2bac11c9cb3e6e9c2e48c5682d12
petervaro <hello@petervaro.com>
2017-05-14 15:11:09 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function main()
+{
+    "use strict";
+    var url       = document.URL,
+        greetings = document.getElementById('greetings');
+
+    if (url &&
+        (url = url.split('#')[1]))
+    {
+        url = decodeURI(url).split('|').join(',<br/>');
+        greetings.innerHTML = 'Kedves<br/>' + url + '!';
+    }
+}
