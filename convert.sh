1051a6e599f644540b2d07fdf16ee2cf0b0d1683
petervaro <hello@petervaro.com>
2017-06-22 08:04:30 +0100

    order-of-service added, and major fixes and updates for the rest

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-IN_DIR="svg";
-OUT_DIR="pdf";
+IN_DIR='svg';
+OUT_DIR='pdf';
 
 mkdir -p "$OUT_DIR";
 for source in $IN_DIR/*.svg;
@@ -8,5 +8,26 @@ do
     source="${source##*/}";
     source="${source%.*}";
     printf "Converting $IN_DIR/$source.svg to $OUT_DIR/$source.pdf\n";
-    convert -density 300 -colorspace CMYK "$IN_DIR/$source.svg" "$OUT_DIR/$source.pdf";
+    convert -density 300x300        \
+            -units PixelsPerInch    \
+            -resize 3579x2551       \
+            -repage 3579x2551       \
+            -colorspace CMYK        \
+            "$IN_DIR/$source.svg"   \
+            "$OUT_DIR/$source.pdf";
+done;
+
+IN_DIR='./';
+for source in $IN_DIR/order-of-service-*.svg;
+do
+    source="${source##*/}";
+    source="${source%.*}";
+    printf "Converting $IN_DIR/$source.svg to $OUT_DIR/$source.pdf\n";
+    convert -density 300x300        \
+            -units PixelsPerInch    \
+            -resize 1819x2551       \
+            -repage 1819x2551       \
+            -colorspace CMYK        \
+            "$IN_DIR/$source.svg"   \
+            "$OUT_DIR/$source.pdf";
 done;
